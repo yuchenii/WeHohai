898803713d4ee6e17ed870fe1cff5fde16d807d4
yuchen <dev.chenyu@gmail.com>
2021-09-18 23:10:53 +0800

    1.使用ColorUI 2.一言

--- request/index.js
+++ request/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yuchen
  * @Date: 2021-09-06 20:59:26
- * @LastEditTime: 2021-09-06 21:44:11
+ * @LastEditTime: 2021-09-18 00:18:00
  * @Description: 
  * @FilePath: \WeHohai\request\index.js
  */
@@ -9,11 +9,21 @@ export const request = (params) => {
     
     //定义公共url
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
+    let url;
+    
+    if (params.baseUrl){
+        url = params.baseUrl + params.url;
+    }else{
+        url = baseUrl + params.url;
+    }
+
+    console.log(url);
+
     return new Promise((resolve, reject) => {
 
         wx.request({
             ...params,
-            url: baseUrl + params.url,
+            url: url,
             success: (result) => {
                 resolve(result);
             },
